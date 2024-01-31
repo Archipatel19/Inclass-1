e42daab8d92613a6d4b1aeb0974c257c9228f376
patel <patel@10.48.58.31>
2024-01-31 18:15:59 -0500

    1

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -1,32 +1,83 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package card;
 
-/**
- * A class that fills a magic hand of 7 cards with random Card Objects
- * and then asks the user to pick a card and searches the array of cards
- * for the match to the user's card. To be used as starting code in ICE 1
- * @author srinivsi
- */
-public class CardTrick {
-    
-    public static void main(String[] args)
-    {
-        Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-        }
-        
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-        // add one luckcard hard code 2,clubs
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+
+/*
+*@modifier : Archi Patel;
+*student number : 991706059;
+*/
+
+class Card {
+    private final int value;
+    private final String suit;
+
+    public Card(int value, String suit) {
+        this.value = value;
+        this.suit = suit;
     }
-    
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Card card = (Card) obj;
+        return value == card.value && suit.equals(card.suit);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + this.value;
+        hash = 17 * hash + Objects.hashCode(this.suit);
+        return hash;
+    }
+}
+
+public class CardTrick {
+
+    public static void main(String[] args) {
+        List<Card> magicHand = new ArrayList<>();
+
+        for (int i = 0; i < 7; i++) {
+            Card c = createRandomCard();
+            magicHand.add(c);
+        }
+
+        Card luckCard = new Card(2, "clubs");
+        magicHand.add(luckCard);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Pick a card value: ");
+        int userValue = scanner.nextInt();
+        System.out.print("Pick a card suit (spades, hearts, diamonds, clubs): ");
+        String userSuit = scanner.next().toLowerCase();
+
+        Card userCard = new Card(userValue, userSuit);
+
+        boolean cardFound = magicHand.contains(userCard);
+
+        if (cardFound) {
+            System.out.println("Congratulations! Your card is in the magic hand.");
+        } else {
+            System.out.println("Sorry, your card is not in the magic hand.");
+        }
+    }
+
+    private static Card createRandomCard() {
+        int randomValue = (int) (Math.random() * 13) + 1; 
+        String[] suits = {"spades", "hearts", "diamonds", "clubs"};
+        String randomSuit = suits[(int) (Math.random() * 4)]; 
+        return new Card(randomValue, randomSuit);
+    }
 }
